4010abe5fc0e2726eaf2d81f7679970c59d2eb48
KoifishVEVO <farhan210604@gmail.com>
2024-03-16 13:37:24 +0700

    menambahkan class DataGeneric

--- tpmodul5_1302220046/tpmodul5_1302220046/Program.cs
+++ tpmodul5_1302220046/tpmodul5_1302220046/Program.cs
@@ -1,2 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+public class HaloGeneric
+{
+
+    public void SapaUser<thing>(thing x)
+    {
+        Console.WriteLine("Halo User " + x);
+    }
+}
+
+public class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
+    }
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        
+        HaloGeneric halo = new HaloGeneric();
+
+        
+        halo.SapaUser(5.65);
+
+
+        DataGeneric<string> data = new DataGeneric<string>("1302220046");
+
+        data.PrintData();
+    }
+}
